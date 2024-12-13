ba431b7e30657f466610988d29c93d4523a82095
Chaiwat S <iwat@users.noreply.github.com>
2024-12-13 11:42:37 -0800

    feat(2015day10): sub-optimal

---
+++ 2015/day10/part2.py
@@ -0,0 +1,22 @@
+# Honestly, I don't think this approach worked in 2015. Machines back then were
+# not this powerful.
+
+line = list('1321131112')
+
+for i in range(50):
+    print(f'{i}:', len(line))
+    new_line = []
+
+    current = line[0]
+    count = 1
+    for c in line[1:]:
+        if c == current:
+            count += 1
+        else:
+            new_line.extend([count, current])
+            current = c
+            count = 1
+    new_line.extend([count, current])
+    line = [str(c) for c in new_line]
+
+print(f'{i + 1}:', len(line))
