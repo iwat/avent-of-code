42a8ac628952780fb76c9d78fe1ecde0131dc3d0
Chaiwat S <iwat@users.noreply.github.com>
2024-12-19 14:28:20 -0800

    feat(2015day17): done

---
+++ 2015/day17/part2.py
@@ -0,0 +1,28 @@
+import sys
+
+
+storage = int(sys.argv[1])
+containers = [int(l.strip()) for l in sys.stdin.readlines()]
+
+
+def possibilities(idxs):
+    remaining = storage - sum([containers[i] for i in idxs])
+    if remaining == 0:
+        print(len(idxs))
+        return 1
+    if remaining < 0:
+        return 0
+
+    min_idx = 0
+    if len(idxs) > 0:
+        min_idx = idxs[-1]
+    poss = 0
+    for i in range(min_idx, len(containers)):
+        if i  in idxs:
+            continue
+        poss += possibilities(idxs + [i])
+    return poss
+
+
+# py part2.py 150 < input.txt | sort -n | uniq -c | head -n 1
+possibilities([])
