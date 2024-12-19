1e7625ebee588b89d9dec477716d0d584512e27a
Chaiwat S <iwat@users.noreply.github.com>
2024-12-19 11:52:24 -0800

    feat(day19): use functools.cache for memoization

--- 2024/day19/part2.py
+++ 2024/day19/part2.py
@@ -1,3 +1,4 @@
+import functools
 import sys
 
 
@@ -5,17 +6,12 @@ available = sys.stdin.readline().strip().split(', ')
 print(available)
 
 
-memo = {}
+@functools.cache
 def solve(current, design):
-    if (current, design) in memo:
-        return memo[(current, design)]
-
     remaining = design[len(current):]
     if len(remaining) == 0:
         if current == design:
-            memo[(current, design)] = 1
             return 1
-        memo[(current, design)] = 0
         return 0
 
     possibilities = 0
@@ -23,7 +19,6 @@ def solve(current, design):
         if remaining[0:len(a)] == a:
             possibilities += solve(current + a, design)
 
-    memo[(current, design)] = possibilities
     return possibilities
 
 
