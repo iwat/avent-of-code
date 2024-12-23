4f0eb8b8a8c6361ff807bb33b13708bd5c25a9ac
Chaiwat S <iwat@users.noreply.github.com>
2024-12-23 10:24:32 -0800

    feat(day23): optimize with more effective memoize technique

--- 2024/day23/part2.py
+++ 2024/day23/part2.py
@@ -1,5 +1,4 @@
 import collections
-import functools
 import sys
 
 Node = collections.namedtuple('Node', ['name', 'neighbors'])
@@ -21,12 +20,18 @@ for line in [l.strip() for l in sys.stdin.readlines()]:
     node_b.neighbors.append(node_a)
 
 
-@functools.cache
+memo_info = {'hits': 0, 'misses': 0}
+
+memo = {}
 def find_mesh(encoded_mesh):
     valid_mesh = [nodes[name] for name in encoded_mesh.split(',')]
 
     last_node = valid_mesh[-1]
     sorted_mesh = ','.join([m.name for m in sorted(valid_mesh, key=lambda m: m.name)])
+    if sorted_mesh in memo:
+        memo_info['hits'] += 1
+        return memo[sorted_mesh]
+    memo_info['misses'] += 1
 
     best_mesh = None
     for candidate_member in last_node.neighbors:
@@ -41,8 +46,10 @@ def find_mesh(encoded_mesh):
                 best_mesh = mesh
 
     if best_mesh is not None:
-        return best_mesh
-    return ','.join(sorted([member.name for member in valid_mesh]))
+        memo[sorted_mesh] = best_mesh
+    else:
+        memo[sorted_mesh] = ','.join(sorted([member.name for member in valid_mesh]))
+    return memo[sorted_mesh]
 
 
 longest_mesh = None
@@ -51,3 +58,4 @@ for node_1 in nodes.values():
     if longest_mesh is None or len(mesh) > len(longest_mesh):
         longest_mesh = mesh
 print(longest_mesh)
+print(memo_info)
